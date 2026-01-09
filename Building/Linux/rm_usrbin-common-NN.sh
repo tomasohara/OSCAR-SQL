7a523a700f1d0cf91d6ab7152b0ff96d34c0cede
Jean-Philippe LEMOINE <untoutseul05@free.fr>
2026-01-09 15:00:19 +0100

    Modify rm_usrbin-common-NN.sh to purge icon on the Desktop file on non English OS.

--- Building/Linux/rm_usrbin-common-NN.sh
+++ Building/Linux/rm_usrbin-common-NN.sh
@@ -1,34 +1,27 @@
+# begin common script
+#
+# modify by untoutseul05 to search local name for Desktop Folder
+# the package now suits the fhs
+
+# application name
+echo "appli_name='$appli_name', SUDO_USER='$SUDO_USER'"
+
+desktop_folder_name=""
 
 if [ ! -z "$SUDO_USER" ]; then
     # find real name of the Desktop folder (Bureau for xubuntu french version)
-    desktop_folder_name="/home/$SUDO_USER/Desktop"
-
-    # si doesn't exist, try to find it translated name 
-    tmp_dir=""
-    if [ ! -d "$desktop_folder_name" ]; then
-        tmp_dir=`cat /home/$SUDO_USER/.config/user-dirs.dirs | grep XDG_DESKTOP_DIR | awk -F= '{print $2}' | awk -F\" '{print $2}' | awk -F\/ '{print $2}'`
-    fi
-
+    desktop_folder_name0="/home/$SUDO_USER/Desktop"
+fi
 
 # if doesn't exist, try to find it translated name
-    translate_file="/home/$SUDO_USER/.config/user-dirs.dirs"
-    if [ -f $translate_file ]; then
-        tmp_dir="" 
-        if [ ! -d "$desktop_folder_name" ]; then
-            tmp_dir=`cat /home/$translate_file | grep XDG_DESKTOP_DIR | awk -F= '{print $2}' | awk -F\" '{print $2}' | awk -F\/ '{print $2}'`
-        fi
-
-       # don't overwrite if translated name or doesn't exist
-        if [ -n "$tmp_dir" ];  then
-            # calculate the full folder
-            tmp_dir_full="/home/${SUDO_USER}/${tmp_dir}"
-            if [ -d "$tmp_dir_full" ]; then
-                desktop_folder_name=$tmp_dir_full
-            fi
-        fi
+translate_file="/home/$SUDO_USER/.config/user-dirs.dirs"
+if [ -f $translate_file ]; then
+    tmp_dir="" 
+    if [ ! -d "$desktop_folder_name" ]; then
+            tmp_dir=$(cat $translate_file | grep XDG_DESKTOP_DIR | awk -F= '{print $2}' | awk -F\" '{print $2}' | awk -F\/ '{print $2}')
     fi
 
-    # don't overwrite if tmp_dir empty or doesn't exist
+    # don't overwrite if translated name or doesn't exist
     if [ -n "$tmp_dir" ];  then
         # calculate the full folder
         tmp_dir_full="/home/${SUDO_USER}/${tmp_dir}"
@@ -37,6 +30,10 @@ if [ ! -z "$SUDO_USER" ]; then
         fi
     fi
 
+    if [ -z "$desktop_folder_name" ]; then
+        desktop_folder_name=$desktop_folder_name0
+    fi
+    
     file="$desktop_folder_name/${appli_name}.desktop"
     if [ -f "$file" ]; then
         rm $file
