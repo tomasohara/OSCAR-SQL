cc80c886514c572db12cdf62bc4ffb53881a7e81
Jean-Philippe LEMOINE <untoutseul05@free.fr>
2026-01-19 07:24:13 +0100

    Modifier mkOSDistDeb-Qt6.sh. strip must be present.

--- Building/Linux/mkOSDistDeb-Qt6.sh
+++ Building/Linux/mkOSDistDeb-Qt6.sh
@@ -215,6 +215,11 @@ mkdir ${temp_folder}/share/applications
 
 # must delete debug symbol in OSCAR binary file - This was replaced with 'base_name'
 #strip -s -o ${temp_folder}/bin/${appli_name} ${build_folder}/oscar/${appli_name}
+# strip must be present : it copies the exe ftom the temp folder to the build folder
+#  without the debug symbol : it is not a simple copy.
+
+# Notice : ${appli_name} must be use : it is ${base_name} added with -test suffix if necessary
+strip -s -o ${temp_folder}/bin/${appli_name} ${build_folder}/oscar/${base_name}
 
 # 2>/dev/null : errors does not appear : we don't care about them
 cp -r ${build_folder}/oscar/Help ${temp_folder}/share/${base_name} 2>/dev/null
