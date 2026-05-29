ce3d9da2f4e7fb8b0a51ac5132d7010544232f5d
Guy Scharf <guy.gitlab@moxis.com>
2026-05-29 16:33:25 -0700

    ts_stats.py: add Empty column for untranslated strings
    Adds a count of unfinished messages that have no translation text at all,
    distinct from unfinished messages that have a mechanical translation pending
    native-speaker review.

--- Tools/ts_stats.py
+++ Tools/ts_stats.py
@@ -10,9 +10,10 @@ def analyse(path):
         content = f.read()
     msg = content.count("<message")
     unf = content.count('type="unfinished"')
+    empty = content.count('type="unfinished"></translation>')
     obs = content.count('type="obsolete"')
     van = content.count('type="vanished"')
-    return msg, unf, obs, van
+    return msg, unf, empty, obs, van
 
 
 def main():
@@ -24,30 +25,31 @@ def main():
         print(f"No .ts files found in: {directory}")
         sys.exit(1)
 
-    fmt = "{:<35} {:>8}  {:>10}  {:>8}  {:>8}  {:>8}"
-    sep = "-" * 85
+    fmt = "{:<35} {:>8}  {:>10}  {:>7}  {:>8}  {:>8}  {:>8}"
+    sep = "-" * 95
 
     print()
     print(f"Translation file statistics: {directory}")
     print(sep)
-    print(fmt.format("File", "Messages", "Unfinished", "Obsolete", "Vanished", "Active"))
+    print(fmt.format("File", "Messages", "Unfinished", "Empty", "Obsolete", "Vanished", "Active"))
     print(sep)
 
-    tot_msg = tot_unf = tot_obs = tot_van = 0
+    tot_msg = tot_unf = tot_empty = tot_obs = tot_van = 0
 
     for fname in files:
-        msg, unf, obs, van = analyse(os.path.join(directory, fname))
+        msg, unf, empty, obs, van = analyse(os.path.join(directory, fname))
         active = msg - obs - van
         tot_msg += msg
         tot_unf += unf
+        tot_empty += empty
         tot_obs += obs
         tot_van += van
         name = os.path.splitext(fname)[0]
-        print(fmt.format(name, msg, unf, obs, van, active))
+        print(fmt.format(name, msg, unf, empty, obs, van, active))
 
     print(sep)
     tot_active = tot_msg - tot_obs - tot_van
-    print(fmt.format(f"TOTAL ({len(files)} files)", tot_msg, tot_unf, tot_obs, tot_van, tot_active))
+    print(fmt.format(f"TOTAL ({len(files)} files)", tot_msg, tot_unf, tot_empty, tot_obs, tot_van, tot_active))
     print()
 
 
