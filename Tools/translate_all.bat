a3e659f8dbb3bd474e09b87635f85152b9d97bf9
Guy Scharf <guy.gitlab@moxis.com>
2026-06-14 02:10:45 -0700

    Add translate_all.bat to run translation script on all non-English languages

---
+++ Tools/translate_all.bat
@@ -0,0 +1,44 @@
+@echo off
+setlocal
+
+set SCRIPT=C:\OSCAR\OSCAR-code\Tools\translate_ts_properly.py
+
+for %%L in (
+    Afrikaans.af
+    Arabic.ar
+    Bulgarian.bg
+    Chinese.zh_CN
+    Chinese.zh_TW
+    Czech.cz
+    Dansk.da
+    Deutsch.de
+    Espaniol.es
+    Espaniol.es_MX
+    Filipino.fil
+    Francais.fr
+    Greek.el
+    Hebrew.he
+    Italiano.it
+    Japanese.ja
+    Korean.ko
+    Magyar.hu
+    Nederlands.nl
+    Norsk.no
+    Polski.pl
+    Portugues.pt
+    Portugues.pt_BR
+    Romanian.ro
+    Russkiy.ru
+    Suomi.fi
+    Svenska.sv
+    Thai.th
+    Turkish.tr
+) do (
+    echo === %%L ===
+    python "%SCRIPT%" %%L
+    if errorlevel 1 echo ERROR on %%L
+    echo.
+)
+
+echo All done.
+endlocal
